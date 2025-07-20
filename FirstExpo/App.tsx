bdfeae00ba449645c658332da9a50b36a36f9084
MisterMAD1 <mistermad.official@gmail.com>
2025-07-20 10:42:55 +0700

    18/7/2568

--- FirstExpo/App.tsx
+++ FirstExpo/App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
+import MenuLayout from "./components/MenuLayout";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <MenuLayout />
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
+const styles = StyleSheet.create({});
